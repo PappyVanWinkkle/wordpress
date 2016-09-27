2045c694bb3474d34bae3566d080c4e60f7f6ed4
Paul <paulchabz@gmail.com>
2016-09-27 21:30:31 +0000

    made some changes to functions.php

--- wp-content/themes/twentynew/functions.php
+++ wp-content/themes/twentynew/functions.php
@@ -1,8 +1,15 @@
 <?php 
+/*
+   =========================
+   include the scripts 
+   =========================
+
+*/
 
   function newtheme_enqueue() {
       wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/twentynew.css', array(), '1.0.0', 'all');
       wp_enqueue_script('customscript', get_template_directory_uri() . '/js/twentynew.js', array(), '1.0.0', true);
+      wp_enqueue_script('jquery');
   }
 
   add_action('wp_enqueue_scripts', 'newtheme_enqueue');
